7bdc1dd68520057dd9aaddfa39d50a32699d0f08
Vishnu Mukundan <vishnumukundan@Vishnus-Air.lan>
2024-08-25 00:45:35 -0400

    updating the test cases

--- test/test_processor.py
+++ test/test_processor.py
@@ -1,14 +1,35 @@
-from setuptools import setup, find_packages
+import pytest
+import pandas as pd
+from data_preprocessing.processor import clean_data
 
-setup(
-    name="nyc_jobs_analysis",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=["pandas", "matplotlib"],
-    entry_points={
-        "console_scripts": [
-            "nyc_jobs_analysis=nyc_jobs_analysis.cli:main",
-        ],
-    },
-    test_suite="tests",
-)
+@pytest.fixture
+def real_data():
+    """
+   Loading my Dataset
+    """
+    return pd.read_csv('NYC_Jobs.csv')
+
+def test_load_data(real_data):
+    """
+    testing to see if the data has been loaded as a dataframe
+    """
+    assert isinstance(real_data,pd.DataFrame)
+
+def test_clean_data(real_data):
+
+    """
+    Testing the clean data function to see if the processed dataframe has been removed of the NaN's, if the columns have been presented 
+    """
+    # Clean the dataset
+    df_cleaned = clean_data(real_data)
+    
+    # Assert that the cleaned DataFrame has fewer rows than the original
+    assert df_cleaned.shape[0] < real_data.shape[0]
+    
+    # Assert that no rows have missing values in the specified columns
+    assert df_cleaned[['Salary Range From', 'Salary Range To', 'Job Category']].isna().sum().sum() == 0
+
+    # Ignore extra columns and focus only on expected ones
+    expected_columns = ['Job ID', 'Agency', 'Posting Type', 'Salary Range From', 'Salary Range To', 'Job Category']
+    df_expected_cleaned = real_data[expected_columns].dropna(subset=expected_columns)
+    pd.testing.assert_frame_equal(df_cleaned[expected_columns], df_expected_cleaned)
